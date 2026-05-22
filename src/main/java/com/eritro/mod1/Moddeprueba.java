21df7b3d88f8e20c9118598ac461b8771d4420dc
alecsq15 <alecsq15@gmail.com>
2026-05-21 19:25:49 -0500

    v1 no speed and test 25 waves

--- src/main/java/com/eritro/mod1/Moddeprueba.java
+++ src/main/java/com/eritro/mod1/Moddeprueba.java
@@ -4,14 +4,17 @@ import org.slf4j.Logger;
 
 import com.eritro.mod1.config.BrutalRaidConfig;
 import com.eritro.mod1.event.BrutalRaidEventHandler;
+import com.eritro.mod1.item.ModItems;
 import com.mojang.logging.LogUtils;
 
+import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(Moddeprueba.MODID)
@@ -26,6 +29,8 @@ public class Moddeprueba {
     public Moddeprueba(IEventBus modEventBus, ModContainer modContainer) {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::addCreative);
+        ModItems.ITEMS.register(modEventBus);
 
         NeoForge.EVENT_BUS.register(new BrutalRaidEventHandler());
 
@@ -37,5 +42,11 @@ public class Moddeprueba {
         LOGGER.info("Brutal raids enabled: Bad Omen {}, max waves {}",
                 BrutalRaidConfig.requiredBadOmenLevel(),
                 BrutalRaidConfig.maxWaves());
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.COMBAT) {
+            event.accept(ModItems.BRUTAL_OMEN_BOTTLE);
+        }
     }
 }
